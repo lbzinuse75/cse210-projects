c6b1adb13a6dd9cf713f6a9cc74e623c2b3694ba
lbzinuse75 <lbzinuse@hotmail.com>
2023-10-09 12:23:50 -0600

    Week 4 Practice Done

---
+++ prepare/Learning03/Fraction.cs
@@ -0,0 +1,36 @@
+using System.Globalization;
+
+public class Fraction
+{
+    private int _top;
+    private int _bottom;
+
+    public Fraction()
+    {
+        _top = 1;
+        _bottom = 1;
+    }
+
+    public Fraction(int number)
+    {
+        _top = number;
+        _bottom = 1;
+    }
+
+    public Fraction(int topNumber, int bottomNumber)
+    {
+        _top = topNumber;
+        _bottom = bottomNumber;
+    }
+
+    public String GetFractionString()
+    {
+        string text = $"{_top}/{_bottom}";
+        return text;
+    }
+
+    public double GetDecimalValue()
+    {
+        return (double)_top / (double) _bottom;
+    }
+}
